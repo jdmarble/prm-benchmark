3b76b1b3b766e49b84a5fa7170c5745b667bf307
James Marble <jmarble@cse.unr.edu>
2011-02-28 12:24:05 -0800

    Add FindOMPLAPP cmake module.

---
+++ CMakeModules/FindOMPLAPP.cmake
@@ -0,0 +1,6 @@
+find_library(OMPLAPP_LIBRARY ompl_app DOC "Location of Open Motion Planning Library app")
+find_path(OMPLAPP_INCLUDE_DIR omplapp/RigidBodyPlanning.h PATH_SUFFIXES "OMPLAPP"
+    DOC "Location of Open Motion Planning Library app header files")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(omplapp DEFAULT_MSG OMPLAPP_LIBRARY OMPLAPP_INCLUDE_DIR)
