9e75ebe1f2453acd980ed87165c0b43f58b68129
James Marble <jmarble@cse.unr.edu>
2011-03-02 11:19:47 -0800

    Add statistics for the main results.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,62 @@
 #include <ompl/geometric/planners/prm/KNearestPRMStar.h>
 #include <ompl/geometric/SimpleSetup.h>
 #include <ompl/config.h>
-
 #include <omplapp/SE3RigidBodyPlanning.h>
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+namespace bacc = boost::accumulators;
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
 #include <iostream>
-#include <numeric>
+#include <limits>
 #include <map>
+#include <numeric>
 
 #include "baswana_randomized_spanner.h"
 
 using namespace ompl;
+
+void distanceStats(const std::vector<double>& d)
+{
+    bacc::accumulator_set<
+        double,
+        bacc::features< bacc::tag::max, bacc::tag::mean > > acc;
+    unsigned int disconnected = 0;
+    foreach(double r, d)
+    {
+        // Count disconnected components
+        if (r == std::numeric_limits<double>::max())
+            ++disconnected;
+        else
+            acc(r);
+    }
+    std::cout << "Warning! Disconnected nodes:" << disconnected << std::endl;
+
+    std::cout << "Mean distance from start: " << bacc::mean(acc) << std::endl;
+    std::cout << "Max. distance from start: " << bacc::max(acc) << std::endl;
+}
+
+template <class Graph>
+void pathStats(const Graph& G, std::vector<double>& d)
+{
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typename boost::graph_traits<Graph>::vertices_size_type
+        n = boost::num_vertices(G);
+
+    // Arbitrarily select the first vertex.
+    Vertex start = *boost::vertices(G).first;
+
+    // Need scratch vector for Dijkstra's
+    std::vector<Vertex> predecessor(n);
+    dijkstra_shortest_paths(G, start,
+        boost::predecessor_map(&predecessor[0]).distance_map(&d[0]));
+
+    // Calculate statistics
+    distanceStats(d);
+}
 
 // For counting the size of a nested vector.
 template <typename T>
@@ -32,8 +78,8 @@ int main(int, char **)
     base::PlannerData plannerData;
     
     // load the robot and the environment
-    std::string robot_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/cubicles_robot.dae";
-    std::string env_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/cubicles_env.dae";
+    std::string robot_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/Easy_robot.dae";
+    std::string env_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/Easy_env.dae";
     setup.setRobotMesh(robot_fname.c_str());
     setup.setEnvironmentMesh(env_fname.c_str());
 
@@ -79,8 +125,14 @@ int main(int, char **)
     typedef geometric::KNearestPRMStar::Edge Edge;
 
     Graph G = planner->as<geometric::KNearestPRMStar>()->getGraph();
-    std::cout << "Graph vertices: " << boost::num_vertices(G) << std::endl;
-    std::cout << "Graph edges: " << boost::num_edges(G) << std::endl;
+    
+    const Graph::vertices_size_type n = boost::num_vertices(G);
+    const Graph::edges_size_type m = boost::num_edges(G);
+    boost::property_map<Graph, boost::edge_weight_t>::type
+            weight = boost::get(boost::edge_weight, G);
+    
+    std::cout << "Graph vertices: " << n << std::endl;
+    std::cout << "Graph edges: " << m << std::endl;
 
     std::map<Edge, bool> spanner_map;
     typedef boost::associative_property_map<std::map<Edge, bool> > spanner_t;
@@ -90,6 +142,37 @@ int main(int, char **)
     
     std::cout << "Spanner edges: " << spanner_map.size() << std::endl;
 
-    Graph spanner;
+    Graph spanner(n);
+    foreach(Edge e, boost::edges(G))
+        if (boost::get(spanner_edge, e) == true)
+            boost::add_edge(boost::source(e, G), boost::target(e, G),
+                    boost::get(weight, e), spanner);
+
+
+    std::cout << "Graph stats..." << std::endl;
+    std::vector<double> d_graph(n);
+    pathStats(G, d_graph);
+
+    std::cout << "Spanner stats..." << std::endl;
+    std::vector<double> d_spanner(n);
+    pathStats(spanner, d_spanner);
+
+    std::cout << "Difference stats..." << std::endl;
+    // d_diff = abs(d_graph - d_spanner)
+    std::vector<double> d_diff(n);
+    std::vector<double>::const_iterator graph_iter(d_graph.begin());
+    std::vector<double>::const_iterator spanner_iter(d_spanner.begin());
+    foreach(double& n, d_diff)
+        if (*graph_iter < std::numeric_limits<double>::max() &&
+            *spanner_iter < std::numeric_limits<double>::max())
+            n = fabs(*graph_iter++ - *spanner_iter++);
+        else
+        {
+            n = std::numeric_limits<double>::max();
+            ++graph_iter;
+            ++spanner_iter;
+        }
+    distanceStats(d_diff);
+    
     return 0;
 }
