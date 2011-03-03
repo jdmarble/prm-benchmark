599087475ec8e2f7c30c469f90ed1081e6b6e6d2
James Marble <jmarble@cse.unr.edu>
2011-03-03 10:35:47 -0800

    Add a non-const set_member_predicate_property_map.

--- src/set_member_predicate_property_map.hpp
+++ src/set_member_predicate_property_map.hpp
@@ -25,4 +25,59 @@ namespace boost {
 
     };
 
+    namespace detail {
+        // Proxy class.
+        template <typename SetContainer>
+        class reference_wrapper
+        {
+        private:
+            typedef typename SetContainer::value_type value_type;
+            SetContainer* m_c;
+            const value_type m_key;
+
+        public:
+
+            reference_wrapper(SetContainer& c, const value_type& key)
+                : m_c(&c), m_key(key) {}
+
+            bool operator=(const bool add)
+            {
+                if (add) m_c->insert(m_key);
+                return add;
+            }
+
+            operator bool() const {
+                return m_c->find(m_key) != m_c->end();
+            }
+        };
+    }// namespace detail
+
+    template <typename SetContainer>
+    class set_member_predicate_property_map
+        : public boost::put_get_helper<
+            bool,
+            set_member_predicate_property_map<SetContainer> >
+    {
+    private:
+
+        SetContainer* m_c;        
+
+    public:
+
+        typedef detail::reference_wrapper<SetContainer> reference;
+        typedef bool value_type;
+        typedef read_write_property_map_tag category;
+        set_member_predicate_property_map() : m_c(NULL) { }
+        set_member_predicate_property_map(SetContainer& c) : m_c(&c) { }
+
+        bool operator[](const typename SetContainer::value_type& k) const {
+            return m_c->find(k) != m_c->end();
+        }
+
+        reference operator[](const typename SetContainer::value_type& k) {
+            return reference(*m_c, k);
+        }
+
+    };
+
 }
